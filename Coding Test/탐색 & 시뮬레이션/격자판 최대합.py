e500a32e81b35c8a96dc68c9392606f4732b9fe5
hantae96 <85547680+hantae96@users.noreply.github.com>
2022-02-02 23:27:37 +0900

    내장모듈 덱큐 사용법

--- Coding Test/탐색 & 시뮬레이션/격자판 최대합.py
+++ Coding Test/탐색 & 시뮬레이션/격자판 최대합.py
@@ -10,6 +10,7 @@ for i in range(n):
     for j in range(n):
         sum1+=a[i][j]
         sum2+=a[j][i]
+        
     if sum1 > largest:
         largest = sum1
     if sum2 > largest:
@@ -23,5 +24,5 @@ for i in range(n):
 
 if sum1 > largest:
         largest = sum1
-    if sum2 > largest:
-        largest = sum2
+if sum2 > largest:
+    largest = sum2
