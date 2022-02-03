b939ac80f1771edf14705bb5fb31d3a74a0c24b1
hantae Choi <hantae96@naver.com>
2022-02-04 00:32:50 +0900

    two_sum_pointer 책 내용이랑 다름 수정해야됨

--- 파이썬 알고리즘 인터뷰/두수의 합_여러 풀이법.py
+++ 파이썬 알고리즘 인터뷰/두수의 합_여러 풀이법.py
@@ -1,5 +1,8 @@
 # 핵심 아이디어 : 모든 조합을 비교하지 않고, 정답 == 타켓에서 첫번째 값을 뺀 값 target - n이 존재하는지 탐색하는 것
 
+from re import I
+
+
 nums = [2,7,11,15]
 target = 9
 # 출력은 [0,1] 
@@ -16,5 +19,36 @@ def twoSum(nums,target):
             # 리턴값으로 조건에 해당하는 num의 인덱스를, 두번째로 조건에 해당하는 인덱스 + 1를 반환한다.
             return [nums.index(num), nums[index+1:].index(complement)+(index+1)]
         
+
+
+def twoSum_dic(nums,target):
+    nums_map = {}
+    for index,num in enumerate(nums):
+        nums_map[num] = index
         
-print(twoSum(nums,target))
+        for i,num in enumerate(nums):
+            # 왜 index != nums_map[target-num]: 면 안되지??
+            if target - num in nums_map and index != nums_map[target-num]:
+                return [i,nums_map[target-num]]
+        
+twoSum_dic(nums,target)
+
+
+nums = [2,7,11,15]
+
+def two_sum_pointer(nums,target):
+    left = 0
+    right = len(nums)-1
+    while left != right:
+        if nums[left] + nums[right] < target:
+            left+=1
+        elif nums[left] + nums[right] > target:
+            right -=1
+        
+        else:
+            return print([left,right])
+            
+        
+        
+        
+two_sum_pointer(nums,target)
