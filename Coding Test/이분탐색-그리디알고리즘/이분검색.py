e68ea749881490555713be5f28e0b1b132f33d88
hantae Choi <hantae96@naver.com>
2022-05-01 19:13:17 +0900

    이분탐색

--- Coding Test/이분탐색-그리디알고리즘/이분검색.py
+++ Coding Test/이분탐색-그리디알고리즘/이분검색.py
@@ -1,47 +1,26 @@
 N = 8
 M = 32
 
-lst = "23 87 65 12 57 32 99 81"
+input1 = "23 87 65 12 57 32 99 81"
 
-lst = list(map(int,lst.split()))
+arr = list(map(int,input1.split()))
 
-lst.sort(reverse=False)
-print(lst)
+arr = sorted(arr)    
 
-left = 0
-right = len(lst)-1
+print(arr)
 
-# left가 right 보다 크면 탐색을 전부 완료한 것
-while left<=right:
-    mid = (left+right)//2
-    
-    if 32 < lst[mid]:
-        right = mid-1
-    
-    if 32 > lst[mid]:
-        left = mid+1
+def binary_search(arr):
+    while arr:
+        mid = len(arr)//2
+        
+        if M == arr[mid]:
+            return mid
+        elif M > arr[mid]:
+            arr = arr[mid+1:]
+        else:
+            arr = arr[:mid]
+            
+print(binary_search(arr))
+
         
     
-# 원하는 조건을 최상단에
-    if 32 == lst[mid]:
-        print(mid+1)
-        break
-    
-    
-    
-import sys
-sys.stdin = open("input.txt","r")
-n,m = map(int,input().split())
-a = list(map(int,input().split()))
-a.sort()
-lt = 0
-rt = n-1
-while lt <=rt:
-    mid = (lt+rt)//2
-    if a[mid] == m:
-        print(mid+1)
-        break
-    elif a[mid]>m:
-        rt = mid-1
-    else:
-        lt = mid+1
