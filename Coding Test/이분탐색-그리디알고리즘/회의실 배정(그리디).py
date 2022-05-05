0a15c784344b54b3b5d1a5db0e1b9a7e4f5f259b
hantae Choi <hantae96@naver.com>
2022-05-05 23:27:45 +0900

    변경사항 추가

--- Coding Test/이분탐색-그리디알고리즘/회의실 배정(그리디).py
+++ Coding Test/이분탐색-그리디알고리즘/회의실 배정(그리디).py
@@ -1,16 +1,37 @@
 
-n = 5
+import sys
 
-meeting = [[1,4],[2,3],[3,5],[4,6],[5,7]]
-    
-meeting.sort(key=lambda x : (x[1],x[0]))
+sys.stdin = open("C:/Users/MIS/Desktop/MIS/TIL-/Coding Test/이분탐색-그리디알고리즘/input.txt","r")
+n = int(input())
+arr = [list(map(int,input().split())) for _ in range(n)]
 
+arr.sort(key = lambda x : x[1])
 
 cnt = 0
 endtime = 0
-for start,end in meeting:
-    if start >= endtime:
+
+for start,end in arr:
+    if start>= endtime:
         cnt+=1
-        endtime = end
-        
+        endtime =end
+
 print(cnt)
+
+
+
+# for i in range(len(arr)):
+#     cnt = 1
+#     endtime=arr[i][1]
+#     for j in range(i,len(arr)):
+#         startime = arr[j][0]
+#         if startime == endtime:
+#             cnt += 1
+#             endtime= arr[j][1]
+    
+#     lst.append(cnt)
+# print(lst)
+# print(max(lst))
+    
+            
+        
+    
