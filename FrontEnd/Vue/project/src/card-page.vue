e5f00b637e26c5a2f2d8bfb05a7bf9fbbb2fd5e6
hantae96 <hantae96@naver.com>
2022-07-04 14:33:37 +0900

    card

--- FrontEnd/Vue/project/src/card-page.vue
+++ FrontEnd/Vue/project/src/card-page.vue
@@ -1,13 +1,10 @@
 <template>
   <!-- 제품 소개 개요 -->
   <div class = "salePage">
-    <div v-for = "(data,i) in dataSet" :key="i">
     <img :src = "dataSet[i].image" class = "room-img">
 <!-- 클릭 이벤트가 발생하면 모달창을 띄운다. 추가적으로 누른거 변수에다가 i를 갱신하면 몇번째 모달창을 눌렀는 지 알 수 있다. -->
-<!-- <h4 @click = "모달창열렸니 = true; 누른거 = i">{{data.title}}</h4> -->
-    
-    <p>{{dataSet[i].price}}원</p>
-    </div>
+        <h4 @click = "모달창열렸니 = true; 누른거 = i">{{data.title}}</h4>   
+        <p>{{dataSet[i].price}}원</p>
   </div>
 </template>
 
