1bd8e5062da516e19b0722116385baaa7f96852e
hantae96 <hantae96@naver.com>
2022-07-25 00:22:39 +0900

    이벤트 버블링

--- FrontEnd/tab.js
+++ FrontEnd/tab.js
@@ -3,60 +3,13 @@
 // div 박스를 보여줘야됨 show 클래스 부착
 
 
-for(var j = 0; j<3; j++){
-
+for(let j = 0; j<3; j++){
     document.querySelectorAll('.tab-button')[j].addEventListener('click',function(){
         for(var i = 0; i<3;i++){
             document.querySelectorAll('.tab-button')[i].classList.remove('orange');
             document.querySelectorAll('.tab-button')[i].classList.remove('show');
-    }
+            }
     document.querySelectorAll('.tab-button')[j].classList.add('orange');
     document.querySelectorAll('.tab-button')[j].classList.add('show');
         })
     }
-
-    // document.querySelectorAll('.tab-button')[1].addEventListener('click',function(){
-    //     for(var i = 0; i<3;i++){
-    //         document.querySelectorAll('.tab-button')[i].classList.remove('orange');
-    //         document.querySelectorAll('.tab-button')[i].classList.remove('show');
-    //     }
-    //     document.querySelectorAll('.tab-button')[1].classList.add('orange');
-    //     document.querySelectorAll('.tab-button')[1].classList.add('show');
-    //     })
-
-    //     document.querySelectorAll('.tab-button')[2].addEventListener('click',function(){
-    //         for(var i = 0; i<3;i++){
-    //             document.querySelectorAll('.tab-button')[i].classList.remove('orange');
-    //             document.querySelectorAll('.tab-button')[i].classList.remove('show');
-    //     }
-    //     document.querySelectorAll('.tab-button')[2].classList.add('orange');
-    //     document.querySelectorAll('.tab-button')[2].classList.add('show');
-    //     })
-
-
-
-
-
-
-// $('.tab-button').eq(0).on('click',function(){
-//     $('.tab-button').removeClass('orange');
-//     $('.tab-button').eq(0).addClass('orange');
-//     $('.tab-button').removeClass('show');
-//     $('.tab-button').eq(0).addClass('show');
-// })
-
-
-// $('.tab-button').eq(1).on('click',function(){
-//     $('.tab-button').removeClass('orange');
-//     $('.tab-button').eq(1).addClass('orange');
-//     $('.tab-button').removeClass('show');
-//     $('.tab-button').eq(1).addClass('show');
-// })
-
-
-// $('.tab-button').eq(2).on('click',function(){
-//     $('.tab-button').removeClass('orange');
-//     $('.tab-button').eq(2).addClass('orange');
-//     $('.tab-button').removeClass('show');
-//     $('.tab-button').eq(2).addClass('show');
-// })
