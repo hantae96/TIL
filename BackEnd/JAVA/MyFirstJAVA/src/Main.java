bf6dc76f0a7e7764aa541b1814b37f4a2378526e
hantae <hantae96@naver.com>
2022-08-15 16:35:07 +0900

    자바 깃 추가

---
+++ BackEnd/JAVA/MyFirstJAVA/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.awt.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+                String input = scanner.nextLine();
+                int num = Integer.parseInt(input);
+                System.out.println((num));
+
+
+
+            }
+        }
