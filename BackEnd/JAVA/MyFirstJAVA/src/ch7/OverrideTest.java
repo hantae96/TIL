1749a93f7ba5de21fc0deded71dbece40e4b2ee8
hantae <hantae96@naver.com>
2022-08-26 23:33:59 +0900

    정리

--- BackEnd/JAVA/MyFirstJAVA/src/ch7/OverrideTest.java
+++ BackEnd/JAVA/MyFirstJAVA/src/ch7/OverrideTest.java
@@ -1,20 +1,39 @@
 package ch7;
 
-class MyPoint3 extends Object {
-    MyPoint3(int x,int y){
-        this.x = x;
-        this.y = y;
-    }
-    int x;
-    int y;
-    public  String toString(){
-        return "x : "+x+", y : "+y;
+import java.util.Scanner;
+
+public class OverrideTest {
+    public static void main(String[] args) {
+
+        System.out.print("게임을 시작 할려면 4아니면 아무거나 ~ 끝낼려면 4 를 누르세요");
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i<3;i++) {
+        int user = scanner.nextInt();
+            int com = (int) (Math.random() * 2);
+            int tmp = user - com;
+
+            String[] arr = {"가위", "바위", "보"};
+
+            System.out.println("나 : " + arr[user]);
+            System.out.println("컴퓨터 : " + arr[com]);
+
+            switch (tmp) {
+                case 1:
+                case -2: {
+                    System.out.println("이김ㅋ");
+                    break;
+                }
+                case 0: {
+                    System.out.println(("비김ㅋ"));
+                    break;
+                }
+                case -1:
+                case 2: {
+                    System.out.println("짐ㅋ");
+                    break;
+                }
+            }
+        }
     }
 }
 
-public class OverrideTest {
-    public static void main(String[] args){
-        MyPoint3 p = new MyPoint3(3,5);
-        System.out.println(p);
-    }
-}
