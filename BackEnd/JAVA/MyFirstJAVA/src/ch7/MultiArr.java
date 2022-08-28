64ac7cf0d047a148102874264ce7f917ac271877
hantae <hantae96@naver.com>
2022-08-28 21:53:36 +0900

    개강

---
+++ BackEnd/JAVA/MyFirstJAVA/src/ch7/MultiArr.java
@@ -0,0 +1,15 @@
+package ch7;
+
+import java.util.Scanner;
+
+public class MultiArr {
+    public static void main(String[] args){
+        int sum = 0 ;
+        int i = 0;
+
+        while((sum += ++i) <= 100){
+            System.out.println(sum);
+        }
+        }
+    }
+
