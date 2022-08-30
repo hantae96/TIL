6b4d1b73e4eaf98e41a312e8adeb4f29a2ab1ba0
hantae <hantae96@naver.com>
2022-08-30 09:22:39 +0900

    TIL

--- BackEnd/JAVA/MyFirstJAVA/src/ch7/MultiArr.java
+++ BackEnd/JAVA/MyFirstJAVA/src/ch7/MultiArr.java
@@ -1,38 +1,20 @@
 package ch7;
-
 import java.util.Scanner;
 
+
 public class MultiArr {
-    public static void main(String[] args){
-        int [][] m1 = {
-                {1,2,3},
-                {4,5,6}
-        };
-
-        int [][] m2 = {
-                {1,2},
-                {3,4},
-                {5,6}
-        };
-
-        final int ROW = m1.length;
-        final int COL = m2[0].length;
-        final int M2_ROW = m2.length;
-
-        int [][] m3 = new int [ROW][COL];
-
-        for(int i = 0; i<ROW ; i++)
-            for(int j = 0; j<COL;j++)
-                for(int k = 0;k<M2_ROW;k++)
-                    m3[i][j] += m1[i][k] * m2[k][j];
-
-        for(int i = 0; i<ROW;i++) {
-            for (int j = 0; j < COL; j++) {
-                System.out.printf("%3d ", m3[i][j]);
-            }
-
-            System.out.println();
-        }
+    public static void main(String[] args) {
+        int sum = 0;
+        sum = factorial(5);
+        System.out.println(sum);
+    }
+    static int factorial(int num) {
+        if (num == 1) {
+            return 1;
+        } else {
+            return num * factorial(num-1);
         }
     }
+}
+
 
