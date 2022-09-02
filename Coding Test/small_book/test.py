90a1f0832c15e6aa4f79d3f72e33f668d0708855
hantae Choi <hantae96@naver.com>
2022-09-02 23:25:04 +0900

    구현 문제

--- Coding Test/small_book/test.py
+++ Coding Test/small_book/test.py
@@ -1,13 +1,28 @@
-num = int(input())
-check_list = [0 for i in range(num+1)]
+import sys
+
+sys.stdin = open("input.txt","r")
+dice_set = []
+n = int(input())
+for i in range(n):
+    dice_set.append(list(map(int,input().split())))
+
 result = []
 
-for i in range(2,num+1):
-    if check_list[i] == 0:
-        result.append(i)
-        j = 1
-        while(i * j <=20):
-            check_list[i*j] = 1
-            j+=1
+for dice in dice_set:
+    cnt = 0
+    same = 0
+    for i in range(len(dice)):
+        for j in range(i+1,len(dice)):
+            if dice[i] != dice[j]:
+                cnt+=1
+            else :
+                same = dice[i]
+        if cnt == 0:
+            result.append(10000 + 1000*same)
+        elif cnt == 1:
+            result.append(1000 + 100 *same)
+        else :
+            result.append(100 * max(dice))
 
-print(len(result))
+print(result)
+print(max(result))
