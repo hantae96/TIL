239fe0bc666d77f821f2c99c80b16d21b26fed83
hantae <hantae96@naver.com>
2022-10-05 23:19:32 +0900

    TIL

--- BackEnd/JAVA/MyFirstJAVA/src/Main.java
+++ BackEnd/JAVA/MyFirstJAVA/src/Main.java
@@ -1,44 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.awt.*;
+import java.io.EOFException;
 import java.util.*;
-import java.util.Scanner;
 
-class Card {
-    String kind;
-    int number;
-    static int width = 100;
-    static int height = 200;
-
-    Card(String kind,int number){
-        this.kind = kind;
-        this.number = number;
-    }
-
-    Card(){
-        this("test",1);
-    }
-
-    void printCard(){
-        System.out.println("모양은"+kind+"숫자는"+number);
-    }
-}
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (num < 10){
+            num *= 10;
+        }
 
-        Card c1 = new Card("ACE",1);
-        Card c2 = new Card("DIA",10);
-        Card c3 = new Card();
+        int count = 1;
+        int res = 0;
+        int a,b,tmp;
+
+        while(num!=res){
+            if (count == 1){
+                a = num/10;
+                b = num%10;
+                tmp = (a+b)%10;
+                res = (num%10)*10 + tmp;
+            }else{
+                a = res/10;
+                b = res%10;
+                tmp = (a+b)%10;
+                res = (res%10)*10 + tmp;
+            }
+            count++;
+        }
 
 
-        c1.printCard();
-        c2.printCard();
-        c3.printCard();
-
-        Card.width = 2000;
-        System.out.println(c3.width);
-
+        System.out.println(count);
     }
 }
--- BackEnd/JAVA/MyFirstJAVA/src/ch11/Ex11_13.java
+++ BackEnd/JAVA/MyFirstJAVA/src/ch11/Ex11_13.java
@@ -1,16 +1,32 @@
 package ch11;
-
 import java.util.*;
-
-class Tv{}
-class Audio{}
-
 
 public class Ex11_13 {
     public static void main(String[] args) {
-        ArrayList<Tv> list = new ArrayList<Tv>();
-        list.add(new Tv());
-//        list.add(new Audio());
-        Tv t = list.get(0);
+        HashMap<String,Student> map = new HashMap<>();
+        map.put("자바왕",new Student("자바왕",1,1,100,100,100));
+
+        Student s = (Student)map.get("자바왕");
+
+        System.out.println(map.get("자바왕"));
+        }
+    }
+
+
+class Student {
+    String name = "";
+    int ban;
+    int no;
+    int kor;
+    int eng;
+    int math;
+
+    Student(String name,int ban,int no,int kor,int eng, int math){
+        this.name = name;
+        this.ban = ban;
+        this.no = no;
+        this.kor = kor;
+        this.eng = eng;
+        this.math = math;
     }
 }
