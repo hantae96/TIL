5fc8fb36959eedcca1eff3154c29d2ca2c56a787
hantae <hantae96@naver.com>
2022-10-09 20:41:11 +0900

    TIL

--- BackEnd/JAVA/MyFirstJAVA/src/Main.java
+++ BackEnd/JAVA/MyFirstJAVA/src/Main.java
@@ -1,39 +1,17 @@
 import java.util.*;
-
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int[][] arr = new int[num][];
-        int[] person = new int[num];
-        double[] result = new double[num];
 
-//        System.out.println(Arrays.toString(arr));
-        for (int i = 0; i < arr.length; i++) {
-            int n = sc.nextInt();
-            person[i] = n;
-            arr[i] = new int[n];
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = sc.nextInt();
-            }
+    long sum(int[] a){
+        int sum = 0
+        for (int i =0; i<a.length;i++){
+            sum += a[i];
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            int sum = 0;
-            for (int j = 0; j < arr[i].length; j++) {
-                sum += arr[i][j];
-            }
-            result[i] = (double) sum / person[i];
-        }
+        return sum;
+    }
 
-        for (int i = 0; i<arr.length;i++){
-            int count = 0;
-            for (int j = 0; j<arr[i].length;j++){
-                if(arr[i][j] > result[i]){
-                    count++;
-                }
-            }
-            System.out.printf("%.3f%%\n",(double)count/person[i]*100);
-        }
+    public static void main(String[] args) throws IOException {
+
     }
 }
