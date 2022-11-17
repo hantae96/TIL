33fe17828ced67f7df138599617dcf9b5f27a9eb
hantae <hantae96@naver.com>
2022-11-18 00:20:22 +0900

    TIL

--- Coding Test/PCCP/주차.py
+++ Coding Test/PCCP/주차.py
@@ -24,7 +24,7 @@ def solution(fees, records):
             in_time[car_number] += change_time(time)
             io_table[car_number] = 1
         else:
-            result_table[car_number] = abs(in_time[car_number] - change_time(time))
+            result_table[car_number] += abs(in_time[car_number] - change_time(time))
             in_time[car_number] = 0
             io_table[car_number] = 0
             
@@ -36,12 +36,12 @@ def solution(fees, records):
     base_time,base_money,add_min,add_money = fees
     
     for i in range(len(result_table)):
-        if result_table[i] >= base_time:
+        if result_table[i] > base_time:
             tmp = base_money
             result_table[i] = result_table[i]-base_time
             tmp += math.ceil(result_table[i]/add_min)*add_money
             result_table[i] = tmp
-        elif result_table[i] < base_time and result_table[i] != 0:
+        elif result_table[i] <= base_time and result_table[i] != 0:
             result_table[i] = base_money
             
     for i in range(len(result_table)):
