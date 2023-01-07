d5ed7040242ea8d879751a2bb6f03dd9a50e7303
hantae <hantae96@naver.com>
2023-01-07 14:50:47 +0900

    쇠막대기..

--- Coding Test/baekjoon/자료구조1/쇠막대기.py
+++ Coding Test/baekjoon/자료구조1/쇠막대기.py
@@ -1,7 +1,7 @@
 import sys
-from collections import deque
-sys.stdin = open("input.txt")
+#sys.stdin = open("input.txt")
 iron_bar = sys.stdin.readline().rstrip()
+
 
 stack = []
 answer = 0
@@ -10,8 +10,11 @@ for i in range(len(iron_bar)):
     if iron_bar[i] == "(":
         stack.append("(")
     elif iron_bar[i] == ")":
-        stack.pop()
-        answer += len(stack)
-
-        
+        if iron_bar[i-1] == "(":
+            stack.pop()
+            answer += len(stack)
+        else:
+            stack.pop()
+            answer +=1
+                
 print(answer)
